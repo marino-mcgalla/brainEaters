280e4acbb199431f7740f7a605505bc63ec38ef2
Marino McGalla <mdm409psu@gmail.com>
2016-05-21 14:51:03 -0700

    added random map generation, player coordinates

--- src/brainEaters/wwwroot/brainEaters.js
+++ src/brainEaters/wwwroot/brainEaters.js
@@ -1,34 +1,55 @@
-//map generation
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext("2d");
-var img = new Image();
-var img2 = new Image();
-img.src = "images/background.png";
-img2.src = "images/maze.png";
-var mapArray = [
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-var background = new Image();
-var maze = new Image();
-background.src = "images/background.png";
-maze.src = "images/maze.png";
+var redWall = new Image();
+var blackWall = new Image();
+var marioX = 0;
+var marioY = 0;
+blackWall.src = "images/background.png";
+redWall.src = "images/maze.png";
+//---------------------------------random map generation---------------------------------------------------------------------------------
+var borderRow = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var hollowRow = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0];
+var randomizedRow = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0]; //for more wall tiles, add more 1's to this array
+//randomize order of tiles in randomizedRow array
+function shuffleArray(randomizedRow) {
+    for (var i = randomizedRow.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = randomizedRow[i];
+        randomizedRow[i] = randomizedRow[j];
+        randomizedRow[j] = temp;
+    }
+    return randomizedRow;
+}
+shuffleArray(randomizedRow);
+//build map
+var mapArray = [];
+//build border rows at first and last row, hollow row every odd row, random row every even row
+for (var i = 0; i < 9; i++) {
+    if (i == 0 || i == 8) {
+        mapArray.push(borderRow);
+    }
+    else if (i % 2 != 0) {
+        mapArray.push(hollowRow);
+    }
+    else if (i % 2 == 0 && i != 8) {
+        shuffleArray(randomizedRow);
+        var randomRow = randomizedRow.slice();
+        randomRow.splice(0, 0, 0);
+        randomRow.splice(11, 0, 0);
+        mapArray.push(randomRow);
+    }
+}
+//draw map in canvas
 var posX = 0;
 var posY = 0;
-img.onload = function () {
+blackWall.onload = function () {
     for (var i = 0; i < mapArray.length; i++) {
         for (var j = 0; j < mapArray[i].length; j++) {
             if (mapArray[i][j] == 0) {
-                context.drawImage(img, posX, posY, 32, 32);
+                context.drawImage(blackWall, posX, posY, 32, 32);
             }
             if (mapArray[i][j] == 1) {
-                context.drawImage(img2, posX, posY, 32, 32);
+                context.drawImage(redWall, posX, posY, 32, 32);
             }
             posX += 32;
         }
@@ -36,32 +57,36 @@ img.onload = function () {
         posY += 32;
     }
 };
-//player movement
+//--------------------------------------------player movement---------------------------------------------------
 var y = 8;
 var x = 8;
 var player = document.getElementById('player');
 player.style.position = 'absolute';
 player.style.top = y + 32 + 'px';
 player.style.left = x + 32 + 'px';
+var currentMario;
 document.addEventListener('keydown', function (e) {
-    //up
-    if (e.keyCode == 38) {
-        y = y - 32;
+    switch (e.keyCode) {
+        case 38:
+            y = y - 32; //up arrow
+            marioY += 1;
+            break;
+        case 40:
+            y = y + 32; //down arrow
+            marioY -= 1;
+            break;
+        case 37:
+            x = x - 32; //left arrow
+            marioX -= 1;
+            break;
+        case 39:
+            x = x + 32; //right arrow
+            marioX += 1;
+            break;
+        default: break;
     }
-    //down
-    if (e.keyCode == 40) {
-        y = y + 32;
-    }
-    //left
-    player.style.top = x + 'px';
-    if (e.keyCode == 37) {
-        x = x - 32;
-    }
-    //right
-    player.style.top = x + 'px';
-    if (e.keyCode == 39) {
-        x = x + 32;
-    }
+    //map current coordinates of the player object when a movement occurs
+    currentMario = "(" + marioX + "," + marioY + ")";
     player.style.top = y + 32 + 'px';
     player.style.left = x + 32 + 'px';
 });
--- src/brainEaters/wwwroot/brainEaters.ts
+++ src/brainEaters/wwwroot/brainEaters.ts
@@ -1,84 +1,100 @@
-﻿//map generation
-let canvas = <HTMLCanvasElement>document.getElementById('myCanvas');
+﻿let canvas = <HTMLCanvasElement>document.getElementById('myCanvas');
 let context = canvas.getContext("2d");
-let img = new Image();
-let img2 = new Image();
+let redWall = new Image();
+let blackWall = new Image();
+let marioX = 0;
+let marioY = 0;
+blackWall.src = "images/background.png";
+redWall.src = "images/maze.png";
 
-img.src = "images/background.png";
-img2.src = "images/maze.png";
+//---------------------------------random map generation---------------------------------------------------------------------------------
+let borderRow = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let hollowRow = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0];
+let randomizedRow = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0]; //for more wall tiles, add more 1's to this array
 
-let mapArray = [
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+//randomize order of tiles in randomizedRow array
+function shuffleArray(randomizedRow) {
+    for (let i = randomizedRow.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = randomizedRow[i];
+        randomizedRow[i] = randomizedRow[j];
+        randomizedRow[j] = temp;
+    }
+    return randomizedRow;
+}
+shuffleArray(randomizedRow);
 
-let background = new Image();
-let maze = new Image();
+//build map
+let mapArray = [];
 
-background.src = "images/background.png";
-maze.src = "images/maze.png";
-
-let posX = 0;
-let posY = 0;
-
-img.onload = () => {
-    for (let i = 0; i < mapArray.length; i++) {
-        for (let j = 0; j < mapArray[i].length; j++) {
-
-            if (mapArray[i][j] == 0) {
-                context.drawImage(img, posX, posY, 32, 32);
-            }
-
-            if (mapArray[i][j] == 1) {
-                context.drawImage(img2, posX, posY, 32, 32);
-            }
-
-            posX += 32;
-        }
-        posX = 0;
-        posY += 32;
+//build border rows at first and last row, hollow row every odd row, random row every even row
+for (let i = 0; i < 9; i++) {
+    if (i == 0 || i == 8) {
+        mapArray.push(borderRow);
+    }
+    else if (i % 2 != 0) {
+        mapArray.push(hollowRow);
+    }
+    else if (i % 2 == 0 && i != 8) {
+        shuffleArray(randomizedRow);
+        let randomRow = randomizedRow.slice();
+        randomRow.splice(0, 0, 0);
+        randomRow.splice(11, 0, 0);
+        mapArray.push(randomRow);
     }
 }
 
-//player movement
+//draw map in canvas
+let posX = 0;
+let posY = 0;
 
+blackWall.onload = () => {
+    for (let i = 0; i < mapArray.length; i++) {
+
+        for (let j = 0; j < mapArray[i].length; j++) {
+            if (mapArray[i][j] == 0) {
+                context.drawImage(blackWall, posX, posY, 32, 32);
+            }
+
+            if (mapArray[i][j] == 1) {
+                context.drawImage(redWall, posX, posY, 32, 32);             
+            }          
+            posX += 32;           
+        }
+        posX = 0;
+        posY += 32;
+    } 
+}
+
+//--------------------------------------------player movement---------------------------------------------------
 let y = 8;
 let x = 8;
 let player = <HTMLImageElement>document.getElementById('player');
 player.style.position = 'absolute'
 player.style.top = y + 32 + 'px';
 player.style.left = x + 32 + 'px';
-
-
+let currentMario;
 
 document.addEventListener('keydown', (e) => {
-    //up
-    if (e.keyCode == 38) {
-        y = y - 32;
+    switch (e.keyCode) {
+        case 38: y = y - 32; //up arrow
+            marioY += 1;
+            break;
+        case 40: y = y + 32; //down arrow
+            marioY -= 1;
+            break;
+        case 37: x = x - 32; //left arrow
+            marioX -= 1;
+            break;
+        case 39: x = x + 32; //right arrow
+            marioX += 1;
+            break;
+        default: break;
     }
-    //down
-    if (e.keyCode == 40) {
-        y = y + 32;
-    }
-    //left
-    player.style.top = x + 'px';
-
-    if (e.keyCode == 37) {
-        x = x - 32;
-    }
-    //right
-    player.style.top = x + 'px';
-
-    if (e.keyCode == 39) {
-        x = x + 32;
-    }
-
+    //map current coordinates of the player object when a movement occurs
+    currentMario = `(${marioX},${marioY})`;
     player.style.top = y + 32 + 'px'; 
     player.style.left = x + 32 + 'px';
 });
+
+
